4e7880b28c6180da6ca53156578a17e6824e3295
Pedro Reis <pedrinzx7896@gmail.com>
2024-04-26 10:27:22 -0300

    web-imobiliario-version4

--- java.js
+++ java.js
@@ -1,8 +1,8 @@
 function clickMenu(){
   if (itens.style.display == 'block') {
     itens.style.display = 'none'
-  } else {
-    itens.style.display = 'block'
-  }
+  } else {  
+    itens.style.display = 'block' 
+  } 
 
 }
